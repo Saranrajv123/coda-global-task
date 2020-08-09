26ffad5c15e3e30b50d758c08c50a7cab6c3ba8f
Saranrajv123 <vsaranraj16@gmail.com>
2020-08-09 21:27:24 +0530

    Updates

---
+++ precache-manifest.83817ef880484854f003dc4caeb24cce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b8545cd861f9a3ec6f8d3349a091e2",
+    "url": "/coda-global-task/index.html"
+  },
+  {
+    "revision": "605606450982d104e171",
+    "url": "/coda-global-task/static/css/main.acff3367.chunk.css"
+  },
+  {
+    "revision": "ccb48e1219cabac0b452",
+    "url": "/coda-global-task/static/js/2.e86dab0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coda-global-task/static/js/2.e86dab0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605606450982d104e171",
+    "url": "/coda-global-task/static/js/main.6e12a873.chunk.js"
+  },
+  {
+    "revision": "c55673c6ac6cd3137075",
+    "url": "/coda-global-task/static/js/runtime-main.5e4f19eb.js"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/coda-global-task/static/media/Metropolis-Black.de55ae52.otf"
+  }
+]);
